e96a2ddb14483a829d30819421a41cbc8ffce618
Abirami2408 <rk.abirami2005@gmail.com>
2026-04-29 21:51:09 +0530

    Day-11 Revised

--- InsertInterval.java
+++ InsertInterval.java
@@ -5,7 +5,6 @@ public class InsertInterval {
        List<int[]> ls = new ArrayList<>();
         int n = intervals.length;
         int i = 0;
-
         // add intervals before overlap
         while (i < n && intervals[i][1] < newInterval[0]) {
             ls.add(intervals[i]);
---
+++ SearchInRotatedSortArr.java
@@ -0,0 +1,34 @@
+public class SearchInRotatedSortArr{
+     public static int search(int[] nums, int target) {
+        int low=0,mid=0,high=nums.length-1;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+            } 
+        }
+      return -1 ;
+    }
+    public static void main(String args[]){
+        int nums[] = {4,5,6,7,0,1,2};
+        int target = 0;
+        int res=search(nums,target);//op:4
+        System.out.println(res);
+    }
+
+
+}
