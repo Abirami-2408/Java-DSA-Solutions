e3b5e42bc45a010cfdf074897b05f5e218fee09e
Abirami2408 <rk.abirami2005@gmail.com>
2026-04-30 16:23:23 +0530

    Day-12 Revised

---
+++ MiniSortedArr.java
@@ -0,0 +1,22 @@
+public class MiniSortedArr {
+    public  static int minimumarr(int arr[]){
+        int n=arr.length;int ans=Integer.MAX_VALUE;
+        int low=0,high=n-1;
+        while(low<=high){
+             int mid=(low+high)/2;
+             if(arr[low]<=arr[mid]){
+                ans=Math.min(ans,arr[low]);
+                low=mid+1;
+             }
+             else{
+                ans=Math.min(ans,arr[mid]);
+                high=mid-1;
+             }
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        int arr[]={4,5,6,7,1,2};
+        System.out.println(minimumarr(arr));
+    }
+}
