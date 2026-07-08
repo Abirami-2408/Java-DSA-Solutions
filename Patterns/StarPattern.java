4472f9cdba85d9338c903c7a877f301f81ab7b15
Abirami2408 <rk.abirami2005@gmail.com>
2026-07-08 17:37:03 +0530

    Add 2nd  Star pattern  Solution

--- Patterns/StarPattern.java
+++ Patterns/StarPattern.java
@@ -1,6 +1,6 @@
 package Patterns;
 public class StarPattern {
-     public  static void pattern1(int n) {
+      static void pattern1(int n) {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 System.out.print("*");
@@ -8,9 +8,20 @@ public class StarPattern {
             System.out.println(" ");
         }
     }
+      static void pattern2(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
     public static void main(String[]args){
         int n=4;
-        pattern1(n);
-        
+         pattern2(n);     
+/* * 
+  ** 
+  *** 
+  **** */      
     }   
 }
