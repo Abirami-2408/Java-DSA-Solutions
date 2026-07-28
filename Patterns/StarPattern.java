88beb97b64149e19fafd24ac63a806798795b408
Abirami2408 <rk.abirami2005@gmail.com>
2026-07-28 17:12:46 +0530

    Characters Printing

--- Patterns/StarPattern.java
+++ Patterns/StarPattern.java
@@ -16,13 +16,39 @@ public class StarPattern {
             System.out.println(" ");
         }
     }
+     static void pattern3(int n) {
+        for(int i=1;i<=n;i++){
+            for(char ch='A';ch<'A'+i;ch++){
+                System.out.print(ch);
+            }
+            System.out.println(" ");
+        }
+    }
+     static void pattern4(int n) {
+        for(int i=1;i<=n;i++){
+            for(char ch='A';ch<='A'+(n-i);ch++){
+                System.out.print(ch +" ");
+            }
+            System.out.println(" ");
+        }
+    }
+ static void pattern5(int n) {
+        for(int i=0;i<n;i++){
+            char ch=(char)('A'+i);
+            for(int j=0;j<=i;j++){
+                System.out.print(ch +" ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    
     public static void main(String[]args){
         int n=4; 
-        pattern1(n);
-         pattern2(n);     
-/* * 
-  ** 
-  *** 
-  **** */      
+       // pattern4(n);
+         //pattern2(n);    
+        // pattern3(n); 
+         pattern5(n);
+    
     }   
 }
