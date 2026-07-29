549f3d699da10f8ccabe8b77d7c0c6da3507ee8e
Abirami2408 <rk.abirami2005@gmail.com>
2026-07-29 17:08:23 +0530

    Right Triangle.java

--- RightTriangle.java
+++ RightTriangle.java
@@ -7,9 +7,42 @@ public class RightTriangle {
             System.out.println();
         }
     }
+     static void pat2(int n){
+         int space=0;
+        for(int i=0;i<n;i++){
+             
+            for(int j=1;j<=n-i;j++){
+                System.out.print("*");
+            }
+            for(int k=1;k<=space;k++){
+                System.out.print(" ");
+            }
+             for(int j=1;j<=n-i;j++){
+                System.out.print("*");
+            }
+           space+=2;
+          System.out.println(" ");
+         
+        }
+        
+
+for(int i=2;i<=n;i++){
+for(int j=1;j<=i;j++){
+    System.out.print("*");
+}
+        for(int k=1;k<=2*(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+    System.out.print("*");
+}
+System.out.println(" ");
+     }
+    }
       public static void main(String[] args){
         int n=5;
-        pat1(n);
+        //pat1(n);
+        pat2(n);
       }
     
 }
