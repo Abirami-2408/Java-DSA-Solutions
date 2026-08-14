d423df0e970983680a166d512886f903e3f81677
Abirami2408 <rk.abirami2005@gmail.com>
2026-08-14 21:44:06 +0530

    Pattern Solved

---
+++ zohoPat.java
@@ -0,0 +1,21 @@
+class zohoPat{
+ public static void func(int num){
+    int tot=0;
+    for(int i=1;i<=7;i++){
+        int col=i;
+        if(i>4) {
+            col=2*num-i;}
+         int n=tot+col;
+        for(int j=1;j<=col;j++){
+            System.out.print(n+" ");n--;
+        }
+        System.out.println();
+        tot+=col;
+    }
+}
+
+    public static void main(String[] args) {
+        int num=4;
+        func(num);
+    }
+}
